5db0cdd09a7185cbf4ec27a20e191eca9fe3c2b7
todayispdxxx <161697635+todayispdxxx@users.noreply.github.com>
2025-02-03 13:55:58 +0800

    Update and rename script.js to script1.js

--- process_bar_chart/script1.js
+++ process_bar_chart/script1.js
@@ -225,7 +225,7 @@ d3.json("https://raw.githubusercontent.com/todayispdxxx/poetry-lyrics/refs/heads
       .attr("height", 10)
       .attr("rx", 4)
       .attr("ry", 4)
-      .attr("fill", "#1CCEAC")
+      .attr("fill", "#FD8C90")
       .style("stroke", "#666666")
       .style("stroke-width", "1px")
       .style("opacity", 0.7)
